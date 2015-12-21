536fa835bffc03664983f0a5cb6675d10d0644aa
lyy840402 <qaazzz1001@kimo.com>
2015-12-21 13:29:55 +0800

    second code model

--- src/A6.java
+++ src/A6.java
@@ -1,9 +1,57 @@
+import java.util.ArrayList;
 
-public class A6 {
+public abstract class A6 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	abstract void doQuestion();
+    abstract void addQuestion(Question question);
+	
 	}
 
-}
+	class Testpaper extends A6{
+		ArrayList<A6>list;
+		
+		public Testpaper (){
+			list = new ArrayList <A6> ( ) ;
+		}
+		void addQuestion(A6 e){
+			list.add (e);
+		}
+		public void op() {
+			int a = 1;
+			a = a+1;
+			for(A6 e: list)
+				e.op();
+		}
+		@Override
+		void doQuestion() {
+			
+		}
+		@Override
+		void addQuestion(Question question) {
+			
+		}
+	} 
+	
+	
+	
+
+	class Question extends A6 {
+		String description;
+
+		public Question(String desc) {
+			this.description = desc;
+		}
+
+		public void print() {
+			char[] desc;
+			System.out.println(desc);
+		}
+
+		@Override
+		public
+		void op() {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+
